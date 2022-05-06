1814f3404eda30553af94d94d92fa70f52e15c86
devbaeb <huihuiich1@gmail.com>
2022-05-06 22:16:10 +0300

    minor fixes

--- chimera/chimera/static/js/chimera.js
+++ chimera/chimera/static/js/chimera.js
@@ -1,15 +1,35 @@
-const note1 = document.querySelector('.cube1')
-const note2 = document.querySelector('.cube2')
-const note3 = document.querySelector('.cube3')
+const toArray = (list) => {
+  return newList = Array.from(list)
+}
 
-const rect1 = note1.getBoundingClientRect()
-const rect2 = note2.getBoundingClientRect()
-const rect3 = note3.getBoundingClientRect()
+const note1 = toArray(document.querySelectorAll('.cube1'))
+const note2 = toArray(document.querySelectorAll('.cube2'))
+const note3 = toArray(document.querySelectorAll('.cube3'))
 
 window.addEventListener('mousemove', function (e) {
   let x = e.clientX
   let y = e.clientY
-  note1.style.transform = `translate(${(x - rect1.left)/120}px, ${(y - rect1.top)/120}px)`
-  note2.style.transform = `translate(${(x - rect2.left)/260}px, ${(y - rect2.top)/120}px)`
-  note3.style.transform = `translate(${(x - rect3.left)/100}px, ${(y - rect3.top)/120}px)`
+
+
+  note1.map(note => {
+    const rect = note.getBoundingClientRect()
+
+    // if (Math.abs(y - rect.top) > 1000) {
+    //   note.style.transition = '500ms'
+    // }
+    // if (Math.abs(rect.top) < this.window.innerHeight){
+    //   note.style.transform = `translate(${(x - rect.left)/120}px, ${(y - rect.top)/120}px)`
+    // }
+    note.style.transform = `translate(${(x - rect.left)/120}px, ${(y - rect.top)/120}px)`
+  })
+
+  note2.map(note => {
+    const rect = note.getBoundingClientRect()
+    note.style.transform = `translate(${(x - rect.left)/260}px, ${(y - rect.top)/120}px)`
+  })
+
+  note3.map(note => {
+    const rect = note.getBoundingClientRect()
+    note.style.transform = `translate(${(x - rect.left)/100}px, ${(y - rect.top)/120}px)`
+  })
 })
