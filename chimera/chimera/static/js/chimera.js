444e8db3e0409772199f01808ef0450da3234c38
devbaeb <huihuiich1@gmail.com>
2022-05-12 20:26:54 +0300

    a few changes to note parallax

--- chimera/chimera/static/js/chimera.js
+++ chimera/chimera/static/js/chimera.js
@@ -7,29 +7,43 @@ const note2 = toArray(document.querySelectorAll('.cube2'))
 const note3 = toArray(document.querySelectorAll('.cube3'))
 
 window.addEventListener('mousemove', function (e) {
-  let x = e.clientX
-  let y = e.clientY
+    let x = e.clientX
+    let y = e.clientY
 
+    note1.map(note => {
+        const rect = note.getBoundingClientRect()
 
-  note1.map(note => {
-    const rect = note.getBoundingClientRect()
+        if (window.scrollY > rect.top - 800){
+            note.style.transform = `translate(${(x - rect.left)/120}px, ${(y - rect.top)/120}px)`
+            setTimeout(() => {
+                note.style.transition = `0ms`
+            }, 1000)
+        }else {
+            note.style.transition = '500ms'
+        }
+    })
 
-    // if (Math.abs(y - rect.top) > 1000) {
-    //   note.style.transition = '500ms'
-    // }
-    // if (Math.abs(rect.top) < this.window.innerHeight){
-    //   note.style.transform = `translate(${(x - rect.left)/120}px, ${(y - rect.top)/120}px)`
-    // }
-    note.style.transform = `translate(${(x - rect.left)/120}px, ${(y - rect.top)/120}px)`
-  })
+    note2.map(note => {
+        const rect = note.getBoundingClientRect()
+        if (window.scrollY > rect.top - 800){
+            note.style.transform = `translate(${(x - rect.left)/120}px, ${(y - rect.top)/120}px)`
+            setTimeout(() => {
+                note.style.transition = `0ms`
+            }, 1000)
+        }else {
+            note.style.transition = '500ms'
+        }
+    })
 
-  note2.map(note => {
-    const rect = note.getBoundingClientRect()
-    note.style.transform = `translate(${(x - rect.left)/260}px, ${(y - rect.top)/120}px)`
-  })
-
-  note3.map(note => {
-    const rect = note.getBoundingClientRect()
-    note.style.transform = `translate(${(x - rect.left)/100}px, ${(y - rect.top)/120}px)`
-  })
+    note3.map(note => {
+        const rect = note.getBoundingClientRect()
+        if (window.scrollY > rect.top - 800){
+            note.style.transform = `translate(${(x - rect.left)/120}px, ${(y - rect.top)/120}px)`
+            setTimeout(() => {
+                note.style.transition = `0ms`
+            }, 1000)
+        }else {
+            note.style.transition = '500ms'
+        }
+    })
 })
